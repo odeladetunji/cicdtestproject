30380c112430d6648e2c8a4d91b73763f8c621d8
odeladetunji <odeladetunji@gmail.com>
2021-07-18 01:43:53 +0100

    second commit

---
+++ cicdtestprojectdev/src/main/java/com/cicdtestprojectdev/cicdtestprojectdev/CicdtestprojectdevApplication.java
@@ -0,0 +1,13 @@
+package com.cicdtestprojectdev.cicdtestprojectdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdtestprojectdevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdtestprojectdevApplication.class, args);
+    }
+
+}
---
+++ cicdtestprojectdev/src/test/java/com/cicdtestprojectdev/cicdtestprojectdev/CicdtestprojectdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdtestprojectdev.cicdtestprojectdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdtestprojectdevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
